201d7322dfa9738537d80cf2237cc5c188133f45
Martin Krasser <krasserm@googlemail.com>
2015-04-06 12:32:42 +0200

    Remove contact page

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -14,10 +14,10 @@ $email_address = $_POST['email'];
 $message = $_POST['message'];
 	
 // Create the email and send the message
-$to = 'martin@martin-krasser.de';
+$to = 'unknown@yourdomain.com';
 $email_subject = "Website Contact Form: $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nMessage:\n$message";
-$headers = "From: noreply@martin-krasser.de\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";	
 mail($to,$email_subject,$email_body,$headers);
 return true;			
